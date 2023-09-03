0cd3249348af9d43133e481f81e32ac2bfcd5ec9
Jimmie Hogklint <hogklint@gmail.com>
2023-09-03 13:20:21 +0000

    Configure logging

--- src/pusher/__main__.py
+++ src/pusher/__main__.py
@@ -1,2 +1,11 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] %(message)s  [%(name)s:%(lineno)s]",
+)
+log = logging.getLogger(__name__)
+
+
 def main():
-    print("am main")
+    log.info("am main")
